6f4ab82cb5f674fe37e7b6e3f9859890cb92cc50
Bezawityfru2 <Bezawityfru21@gmail.com>
2026-07-10 06:00:21 +0300

    fix mysql connection pool

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -5,12 +5,15 @@ console.log("DB_PORT:", process.env.DB_PORT);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
 
-const dbConnection = mysql.createConnection({
+const dbConnection = mysql.createPool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 (async () => {
